92e20c4ddf564234609d8b0e8aabc3328c63cc3e
andy1764 <chen1764@gmail.com>
2023-03-28 10:48:59 -0400

    initial documentation

--- R/pare.R
+++ R/pare.R
@@ -1,32 +1,56 @@
-pare <- function(Y, formula, data = NULL, embed, type = c("MDS", "dist"),
-                 out = FALSE, ...) {
+#' PAREs: PARtial Embeddings for deconfounded dimension reduction
+#'
+#' `pare` implements a framework for removing nuisance covariates from any
+#' distance-based dimension reduction method including \link[Rtsne]{Rtsne} and
+#' \link[umap]{umap}. The `pare` function takes either data or a distance matrix
+#' as an input then passes a deconfounded distance matrix to a dimension
+#' reduction method. The output is a PARE with respect to the specified
+#' confounder.
+#'
+#' @param Y Input data or distance matrix. If `Y` does not have class "dist",
+#'   then Euclidean distance is used to construct the distance matrix
+#' @param formula Formula for regressing out confounding variables
+#' @param data Data frame containing variables in the model specified by `formula`
+#' @param embed Dimension reduction method. Must be a function such as
+#'   \link[Rtsne]{Rtsne} or \link[umap]{umap}
+#' @param type Type of input into `embed`. Refer to documentation of embedding
+#'   function
+#' @param ... Additional arguments passed to `embed`
+#'
+#' @return `pare` returns the output of `embed` and the format depends on the
+#'   method specified
+#' @export
+#'
+#' @examples
+#' pare(iris[,-5], ~ Species, data = iris, cmdscale, type = "dist")
+pare <- function(Y, formula, data = NULL, embed, type = c("MDS", "dist"), ...) {
   type <- match.arg(type)
-  n <- dim(Y)[1]
-  D <- dist(Y)
+
+  if (inherits(Y, "dist")) {
+    n <- attr(D, "Size")
+    D <- Y
+  } else {
+    n <- dim(Y)[1]
+    D <- dist(Y)
+  }
 
   switch(type,
     "MDS" = {
       Z <- cmdscale(D, add = TRUE, k = n-2)$points
       z_formula <- update(formula, Z ~ .)
+      data$Z <- Z
 
       pZ <- lm(z_formula, data)$residuals
 
-      if (out) { return(pZ) }
-
-      return(
-        do.call(embed, list(pZ, ...))
-      )
+      return(do.call(embed, list(pZ, ...)))
     },
     "dist" = {
+      D <- as.matrix(D)
       X <- model.matrix(formula, data = data)
       H <- X %*% solve(t(X) %*% X) %*% t(X)
       pD <- as.dist((diag(n) - H) %*% D)
 
-      if (out) { return(pD) }
-
-      return(
-        do.call(embed, list(pD, ...))
-      )
+      return(do.call(embed, list(pD, ...)))
     }
   )
 }
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# PARE
- PARtial Embeddings for deconfounded dimension reduction
+PARE: PARtial Embeddings for deconfounded dimension reduction
+================
+
+**Maintainer**: Andrew Chen, <andrewac@pennmedicine.upenn.edu>
+
+**License**: Artistic License 2.0
+
+Partial embeddings (PAREs) provide a framework for removal of
+confounding effects from any distance-based dimension reduction method
+including $t$-SNE, UMAP, Laplacian Eigenmaps, diffusion map embeddings,
+and others. For Euclidean distances, PAREs are equivalent to applying
+dimension reduction to the principal coordinates after regressing out
+nuisance covariates.
+
+## 1. Installation
+
+The R package can be installed via devtools by running the following
+code
+
+    # install.packages("devtools")
+    devtools::install_github("andy1764/PARE", build_vignettes = TRUE)
+
+Then, you can load this package via
+
+    library(PARE)
+
+## 2. Usage
+
+`PARE` leverages the existing R implementations for dimension reduction
+methods. Below is an example call for partial UMAP in the `iris`
+dataset, which removes the species effects:
+
+``` r
+library(PARE)
+library(umap)
+
+pe <- pare(iris[,-5], ~ Species, data = iris, umap)
+```
+
+A vignette is provided for the `pare` function, which also covers other
+dimension reduction methods
+
+    vignette("pare")
+
+## 3. Citations
+
+If using the PARE methodology, please cite the following paper:
+
+> Chen, A. A., Clark, K., Dewey, B., DuVal, A., Pellegrini, N., Nair,
+> G., Jalkh, Y., Khalil, S., Zurawski, J., Calabresi, P., Reich, D.,
+> Bakshi, R., Shou, H., Shinohara, R. T., Initiative, the A. D. N., &
+> Cooperative, the N. A. I. in M. S. (2023). Deconfounded Dimension
+> Reduction via Partial Embeddings (p. 2023.01.10.523448). bioRxiv.
+> <https://doi.org/10.1101/2023.01.10.523448>
